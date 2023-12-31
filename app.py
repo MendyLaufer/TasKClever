65fa0d796f8a20c2c3db20bd4f73c59786aab275
Mendy Laufer <laufermendy@gmail.com>
2023-12-31 23:31:38 +0200

    SQLite database with models

--- app.py
+++ app.py
@@ -1,12 +1,32 @@
 ﻿# -*- coding: utf-8 -*-
-
-from flask import Flask
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'  # Use SQLite for simplicity
+db = SQLAlchemy(app)
+
+# Define User and Task models
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    tasks = db.relationship('Task', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}')"
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Task('{self.content}')"
 
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def home():
+    users = User.query.all()
+    return render_template('index.html', users=users)
 
 if __name__ == '__main__':
     app.run(debug=True)
